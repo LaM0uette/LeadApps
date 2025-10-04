6ed25b74c21d8f794e949b4daf5bd91f6917b250
LaM0uette <lam0uette.dev@gmail.com>
2025-10-04 22:45:00 +0200

    add DTO

---
+++ TopDeck/TopDeck.Contracts/DTO/DeckSuggestionOutputDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TopDeck.Contracts.DTO;
+
+public record DeckSuggestionOutputDTO(
+    [property: JsonPropertyName("id")] int Id,
+    [property: JsonPropertyName("suggestor")] UserInputDTO Suggestor,
+    [property: JsonPropertyName("deck")] DeckInputDTO Deck,
+    [property: JsonPropertyName("addedCardIds")] ICollection<int> AddedCardIds,
+    [property: JsonPropertyName("removedCardIds")] ICollection<int> RemovedCardIds,
+    [property: JsonPropertyName("addedEnergyIds")] ICollection<int> AddedEnergyIds,
+    [property: JsonPropertyName("removedEnergyIds")] ICollection<int> RemovedEnergyIds,
+    [property: JsonPropertyName("likes")] int Likes,
+    [property: JsonPropertyName("createdAt")] DateTime CreatedAt,
+    [property: JsonPropertyName("updatedAt")] DateTime UpdatedAt
+);
