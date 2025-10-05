c40cfd9095d42491d7d98aaa6f3264abe02e6a72
LaM0uette <lam0uette.dev@gmail.com>
2025-10-05 10:38:10 +0200

    handle full login with oauth

--- TopDeck/TopDeck.Client/Program.cs
+++ TopDeck/TopDeck.Client/Program.cs
@@ -3,6 +3,10 @@ using TopDeck.Shared.UIStore;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthenticationStateDeserialization();
+
 // Services
 builder.Services.AddSingleton<UIStore>();
 
--- TopDeck/TopDeck/Program.cs
+++ TopDeck/TopDeck/Program.cs
@@ -1,10 +1,23 @@
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using TopDeck.Components;
 using TopDeck.Shared.UIStore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services
+    .AddAuth0WebAppAuthentication(options => {
+        options.Domain = builder.Configuration["Auth0:Domain"];
+        options.ClientId = builder.Configuration["Auth0:ClientId"];
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();
+
 builder.Services.AddRazorComponents()
-    .AddInteractiveWebAssemblyComponents();
+    .AddInteractiveWebAssemblyComponents()
+    .AddAuthenticationStateSerialization();
 
 // Services
 builder.Services.AddSingleton<UIStore>();
@@ -23,8 +36,31 @@ else
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAntiforgery();
+
+
+app.MapGet("/Account/Login", async Task (HttpContext httpContext, string returnUrl = "/") =>
+{
+    AuthenticationProperties authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+        .WithRedirectUri(returnUrl)
+        .Build();
+
+    await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+
+app.MapGet("/Account/Logout", async httpContext =>
+{
+    AuthenticationProperties authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+        .WithRedirectUri("/")
+        .Build();
+
+    await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
+
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
