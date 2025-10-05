2abe88f0b3ef4cadc5d27360d6c03f7966f2a591
LaM0uette <lam0uette.dev@gmail.com>
2025-10-05 23:02:54 +0200

    add LmButton

--- TopDeck/TopDeck.Client/Pages/Home.razor.cs
+++ TopDeck/TopDeck.Client/Pages/Home.razor.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using LocalizedComponent;
 
 namespace TopDeck.Client.Pages;
 
-public class HomeBase : ComponentBase
+public class HomeBase : LocalizedComponentBase
 {
-    
+    #region Statements
+
+    //
+
+    #endregion
+
+    #region Methods
+
+    protected void OnLoginClicked()
+    {
+        
+    }
+
+    #endregion
 }
---
+++ TopDeck/TopDeck.Shared/Modules/Components/Buttons/LmButton.razor.cs
@@ -0,0 +1,18 @@
+﻿using LocalizedComponent;
+using Microsoft.AspNetCore.Components;
+
+namespace TopDeck.Shared.Components;
+
+public class LmButtonBase : LocalizedComponentBase
+{
+    #region Statements
+
+    [Parameter] public string style { get; set; } = string.Empty;
+    
+    [Parameter] public string Width { get; set; } = "auto";
+    [Parameter] public string Height { get; set; } = "36px";
+    [Parameter] public EventCallback Clicked { get; set; }
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    #endregion
+}
