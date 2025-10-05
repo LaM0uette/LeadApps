ad3f05fbdb71b741bf6db6548028162b25cb626a
LaM0uette <lam0uette.dev@gmail.com>
2025-10-05 15:50:43 +0200

    create AuthPanel module

---
+++ TopDeck/TopDeck.Shared/Modules/AuthPanel/AuthPanel.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace AuthPanel;
+
+public class AuthPanelBase : ComponentBase
+{
+    #region Statements
+
+    [CascadingParameter]
+    private Task<AuthenticationState>? authenticationState { get; set; }
+
+    protected string Username = "";
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (authenticationState is not null)
+        {
+            AuthenticationState state = await authenticationState;
+            Username = state.User.Identity?.Name ?? string.Empty;
+        }
+    }
+
+    #endregion
+}
