9f30b7a3c264cbd8c85a354bd80fcb53d582765b
LaM0uette <lam0uette.dev@gmail.com>
2025-10-07 19:19:18 +0200

    create deck header

---
+++ TopDeck/TopDeck.Shared/Components/Buttons/LikeButton.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TopDeck.Shared.Components;
+
+public class LikeButtonBase : ComponentBase
+{
+    #region Statements
+
+    [Parameter] public int Likes { get; set; }
+    
+    protected bool IsLiked;
+
+    #endregion
+
+    #region Methods
+
+    
+
+    #endregion
+}
--- TopDeck/TopDeck.Shared/Components/DeckView/DeckView.razor.cs
+++ TopDeck/TopDeck.Shared/Components/DeckView/DeckView.razor.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using LocalizedComponent;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using TopDeck.Domain.Models;
 
 namespace TopDeck.Shared.Components;
 
-public class DeckViewBase : ComponentBase
+public class DeckViewBase : LocalizedComponentBase
 {
     #region Statements
 
     [Parameter, EditorRequired] public required Deck Deck { get; set; }
+
+    protected string DeckCode = string.Empty;
+    
+    [Inject] private IJSRuntime _js { get; set; } = null!;
+
+    protected override void OnParametersSet()
+    {
+        DeckCode = Deck.Code;
+    }
 
     #endregion
 
@@ -17,6 +28,17 @@ public class DeckViewBase : ComponentBase
     {
         int id = energieIds.FirstOrDefault();
         return id <= 0 ? "energy-none" : $"energy-{id}";
+    }
+    
+    protected async Task CopyCode()
+    {
+        await _js.InvokeVoidAsync("navigator.clipboard.writeText", Deck.Code);
+        DeckCode = Localizer.Localize("feedback.text.copied");
+        StateHasChanged();
+
+        await Task.Delay(1500);
+        DeckCode = Deck.Code;
+        StateHasChanged();
     }
 
     #endregion
