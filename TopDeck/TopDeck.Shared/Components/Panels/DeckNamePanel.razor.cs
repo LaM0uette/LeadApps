61ec7fae8310fb4ff13ad26fc2d8c58811ca72fa
LaM0uette <lam0uette.dev@gmail.com>
2025-10-08 21:15:13 +0200

    add DeckNamePanel component

---
+++ TopDeck/TopDeck.Shared/Components/Panels/DeckNamePanel.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TopDeck.Shared.Components;
+
+public class DeckNamePanelBase : ComponentBase
+{
+    #region Statements
+
+    [Parameter, EditorRequired] public required string Name { get; set; }
+    
+    #endregion
+}
