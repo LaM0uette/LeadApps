88a029c26a76979ab7d63c6487439f1c25a19196
LaM0uette <lam0uette.dev@gmail.com>
2025-10-12 17:14:00 +0200

    clean

--- TopDeck/TopDeck.Api/Services/IDeckSuggestionLikeService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TopDeck.Contracts.DTO;
-
-namespace TopDeck.Api.Services;
-
-public interface IDeckSuggestionLikeService
-{
-    Task<DeckSuggestionLikeOutputDTO?> CreateAsync(DeckSuggestionLikeInputDTO dto, CancellationToken ct = default);
-    Task<bool> DeleteAsync(int deckSuggestionId, int userId, CancellationToken ct = default);
-}
