36b50556c188116d439b6dde58b3058217c8ceda
LaM0uette <lam0uette.dev@gmail.com>
2025-10-18 10:13:01 +0200

    add add deck button

---
+++ TopDeck/TopDeck.Client/Pages/DeckDetailsEditPage.razor.cs
@@ -0,0 +1,30 @@
+﻿using TopDeck.Shared.Components;
+
+namespace TopDeck.Client.Pages;
+
+public class DeckDetailsEditPagePresenter : PresenterBase
+{
+    #region Statements
+    
+    protected readonly Dictionary<int, string> EnergyTypes = new()
+    {
+        { 1, "Grass" },
+        { 2, "Fire" },
+        { 3, "Water" },
+        { 4, "Lightning" },
+        { 5, "Psychic" },
+        { 6, "Fighting" },
+        { 7, "Darkness" },
+        { 8, "Metal" },
+        { 9, "Dragon" },
+        { 10, "Colorless" }
+    };
+
+    #endregion
+
+    #region Methods
+
+    //
+
+    #endregion
+}
---
+++ TopDeck/TopDeck.Shared/Components/Deck/DeckItemAddButton.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using TCGPCardRequester;
+using TopDeck.Domain.Models;
+using TopDeck.Shared.Models.TCGP;
+
+namespace TopDeck.Shared.Components;
+
+public class DeckItemAddButtonPresenter : PresenterBase
+{
+    #region Statements
+    
+    [Inject] protected NavigationManager NavigationManager { get; set; } = null!;
+
+    #endregion
+
+    #region Methods
+
+    //
+
+    #endregion
+}
