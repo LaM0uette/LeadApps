5bbf4c88db5dfdb16b30e2b715d60b5856d7e0ba
LaM0uette <lam0uette.dev@gmail.com>
2025-10-18 17:11:06 +0200

    show cards

--- TopDeck/TopDeck.Client/Pages/DeckDetailsEditPage.razor.cs
+++ TopDeck/TopDeck.Client/Pages/DeckDetailsEditPage.razor.cs
@@ -1,4 +1,6 @@
-﻿using TopDeck.Shared.Components;
+﻿using Microsoft.AspNetCore.Components;
+using TCGPCardRequester;
+using TopDeck.Shared.Components;
 using TopDeck.Shared.Models.TCGP;
 
 namespace TopDeck.Client.Pages;
@@ -34,8 +36,41 @@ public class DeckDetailsEditPagePresenter : PresenterBase
     protected string DeckName { get; set; } = "Nom du Deck";
     protected IReadOnlyList<TCGPCard> TCGPHighlightedCards { get; set; } = [];
     protected IReadOnlyList<TCGPCard> TCGPCards { get; set; } = [];
+    protected IReadOnlyList<TCGPCard> TCGPAllCards { get; set; } = [];
     
     protected Tab CurrentTab { get; set; } = Tab.Cards;
+    
+    [Inject] private ITCGPCardRequester _tcgpCardRequester { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        TCGPCardsRequest deckRequest = new(
+            [
+                new TCGPCardRequest("A1", 1),
+                new TCGPCardRequest("A1", 2),
+                new TCGPCardRequest("A1", 3),
+                new TCGPCardRequest("A1", 4),
+                new TCGPCardRequest("A1", 5),
+                new TCGPCardRequest("A1", 6),
+                new TCGPCardRequest("A1", 7),
+                new TCGPCardRequest("A1", 8),
+                new TCGPCardRequest("A1", 9),
+                new TCGPCardRequest("A1", 10),
+                new TCGPCardRequest("A1", 11),
+                new TCGPCardRequest("A1", 12),
+                new TCGPCardRequest("A1", 13),
+                new TCGPCardRequest("A1", 14),
+                new TCGPCardRequest("A1", 15),
+                new TCGPCardRequest("A1", 16),
+                new TCGPCardRequest("A1", 17),
+                new TCGPCardRequest("A1", 18),
+                new TCGPCardRequest("A1", 19),
+                new TCGPCardRequest("A1", 20)
+            ]);
+        
+        TCGPAllCards = await _tcgpCardRequester.GetTCGPCardsByRequestAsync(deckRequest, loadThumbnail:true);
+        TCGPCards = TCGPAllCards.Take(17).ToList();
+    }
 
     #endregion
 
