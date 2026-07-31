211d79b0121652dedefca2cc1cd22ef67932fea4
LaM0uette <lam0uette.dev@gmail.com>
2026-08-01 00:38:53 +0200

    up

---
+++ TopDeck/TopDeck.Shared/Modules/DeckSharing/Energy.cs
@@ -0,0 +1,13 @@
+namespace DeckSharing;
+
+public enum Energy
+{
+    Grass = 1,
+    Fire = 2,
+    Water = 3,
+    Lightning = 4,
+    Psychic = 5,
+    Fighting = 6,
+    Darkness = 7,
+    Metal = 8
+}
